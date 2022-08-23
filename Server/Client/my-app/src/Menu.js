458b9d2f7767f51da2c267b3e2f01bcbae6cb517
chileoro0000 <g.rodrguezcampos@uandresbello.edu>
2022-08-22 21:04:57 -0400

    Primer

---
+++ Server/Client/my-app/src/Menu.js
@@ -0,0 +1,13 @@
+export function Menu(){
+    return <header>
+        <h1>Fukusuke Sushi-Delivery</h1>
+        <nav className="Menu">
+            <ul className="Menu">
+                <li>Menu<a href="#"/></li>
+                <li>HandRolls<a href="#"/></li>
+                <li>Piezas<a href="#"/></li>
+                <li>Sobre nosotros<a href="#"/></li>
+            </ul>
+        </nav>
+    </header>
+  }
---
+++ Server/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+//Conexion base de datos
+const mongoose = require("mongoose")
+const user = 'proyectosushi';
+const password = 'QWmbuGcjisjYg3vI';
+const name = "DataBase-Sushi";
+const uri = `mongodb+srv://${user}:${password}@cluster0.63v1qkt.mongodb.net/${name}?retryWrites=true&w=majority`;
+
+mongoose
+	.connect(uri)
+	.then(() => console.log("conectado a BBDD"))
+
+//modelo
+const Producto = require("./models/productos")
+
+//ruteo
+app.get('/productos', async (req, res) => {
+
+	try{
+		const arrayProductosDB = await Producto.find()
+		res.json(arrayProductosDB)
+
+	} catch (error){
+		console.log(error)
+	}
+})
+
+app.listen(5005, () => console.log("Servidor levantado"))
+
+
