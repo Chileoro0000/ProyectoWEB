6451c759748e9ffae17e270cf51e4d4dba1a320b
yikezzz <66960563+jeanyiquez@users.noreply.github.com>
2022-09-02 21:03:00 -0400

    Add files via upload

--- ProyectoWEB/client/src/pages/SobreNosotros/SobrePage.jsx
+++ ProyectoWEB/client/src/pages/SobreNosotros/SobrePage.jsx
@@ -9,12 +9,14 @@ const SobreNosotrosPage = () =>{
             <section>
                 <h1 id="TextoSobre">Sobre Nosotros</h1>
                 <p classname ="Parrafo" >Somos una empresa dedicada a la preparación y venta de sushi en nuestro propio local, estamos ubicados en la comuna de Maipú, con 
-                una capacidad máxima de atención de 12 personas. La atención se realiza en forma personalizada donde los clientes eligen los productos a consumir previa 
-                lectura de la carta confeccionada para este objetivo. La segunda modalidad es la venta de comida para llevar, en donde usted realiza su pedido en mesón 
+                una capacidad máxima de atención de 12 personas.<br/> La atención se realiza en forma personalizada donde los clientes eligen los productos a consumir previa 
+                lectura de la carta confeccionada para este objetivo.<br/> La segunda modalidad es la venta de comida para llevar, en donde usted realiza su pedido en mesón 
                 para llevarlo a su domicilio.</p>
                 {/* <hr><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3326.9214714716177!2d-70.75985088431199!3d-33.503420180759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c31c2a7229b3%3A0xc984e90fc002568f!2sNiu%20Sushi!5e0!3m2!1ses!2scl!4v1661654776333!5m2!1ses!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></hr> */}
             </section>
-            <Footer></Footer>
+            <div id="foot">
+                <Footer/>
+            </div>
         </div>
     )
 }
