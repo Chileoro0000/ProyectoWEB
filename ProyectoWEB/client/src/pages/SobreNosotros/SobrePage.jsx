d3555ccf2c73b305ab428ac2cadd23c37f757012
yikezzz <66960563+jeanyiquez@users.noreply.github.com>
2022-09-19 01:14:16 -0300

    Add files via upload

--- ProyectoWEB/client/src/pages/SobreNosotros/SobrePage.jsx
+++ ProyectoWEB/client/src/pages/SobreNosotros/SobrePage.jsx
@@ -14,14 +14,13 @@ const SobreNosotrosPage = () => {
                     para llevarlo a su domicilio.</p>
                 {/* <hr><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3326.9214714716177!2d-70.75985088431199!3d-33.503420180759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c31c2a7229b3%3A0xc984e90fc002568f!2sNiu%20Sushi!5e0!3m2!1ses!2scl!4v1661654776333!5m2!1ses!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></hr> */}
             </section>
-
-
-
-            <div id="foot">
-
-            </div>
-            
         </div>
+
+        <div class="row">
+
+        <iframe class ="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13304.930052751579!2d-70.79176996044917!3d-33.521339899999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662dd250eb3bcbb%3A0x2b992ee2c8602356!2sDelivery%20Sushi%20Maipu!5e0!3m2!1ses!2scl!4v1663558300991!5m2!1ses!2scl"></iframe>
+
+    </div>
         <div id="Containersobre">
             <div class="card-sobre">
                 <div class="content-sobre">
@@ -65,109 +64,11 @@ const SobreNosotrosPage = () => {
                     </div>
                 </div>
             </div>
-            <div class="card-sobre">
-                <div class="content-sobre">
-                    <div class="front-sobre">
-                        <h3 class="title-sobre">Dato 1</h3>
-                        <p class="subtitle-sobre"></p>
-                    </div>
-
-                    <div class="back-sobre">
-                        <p class="description-sobre">
-                            Cool description so you can read it too my friend
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="card-sobre">
-                <div class="content-sobre">
-                    <div class="front-sobre">
-                        <h3 class="title-sobre">Dato 1</h3>
-                        <p class="subtitle-sobre"></p>
-                    </div>
-
-                    <div class="back-sobre">
-                        <p class="description-sobre">
-                            Cool description so you can read it too my friend
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="card-sobre">
-                <div class="content-sobre">
-                    <div class="front-sobre">
-                        <h3 class="title-sobre">Dato 1</h3>
-                        <p class="subtitle-sobre"></p>
-                    </div>
-
-                    <div class="back-sobre">
-                        <p class="description-sobre">
-                            Cool description so you can read it too my friend
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="card-sobre">
-                <div class="content-sobre">
-                    <div class="front-sobre">
-                        <h3 class="title-sobre">Dato 1</h3>
-                        <p class="subtitle-sobre"></p>
-                    </div>
-
-                    <div class="back-sobre">
-                        <p class="description-sobre">
-                            Cool description so you can read it too my friend
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="card-sobre">
-                <div class="content-sobre">
-                    <div class="front-sobre">
-                        <h3 class="title-sobre">Dato 1</h3>
-                        <p class="subtitle-sobre"></p>
-                    </div>
-
-                    <div class="back-sobre">
-                        <p class="description-sobre">
-                            Cool description so you can read it too my friend
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="card-sobre">
-                <div class="content-sobre">
-                    <div class="front-sobre">
-                        <h3 class="title-sobre">Dato 1</h3>
-                        <p class="subtitle-sobre"></p>
-                    </div>
-
-                    <div class="back-sobre">
-                        <p class="description-sobre">
-                            Cool description so you can read it too my friend
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="card-sobre">
-                <div class="content-sobre">
-                    <div class="front-sobre">
-                        <h3 class="title-sobre">Dato 1</h3>
-                        <p class="subtitle-sobre"></p>
-                    </div>
-
-                    <div class="back-sobre">
-                        <p class="description-sobre">
-                            Cool description so you can read it too my friend
-                        </p>
-                    </div>
-                </div>
-            </div>
+            
         </div>
         
         <Footer />
     </>
     )
 }
-
 export default SobreNosotrosPage
