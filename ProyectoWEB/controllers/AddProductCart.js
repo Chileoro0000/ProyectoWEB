2c2da634805a129f61f74a8dc494b613858e7e1b
Chileoro0000 <101371413+Chileoro0000@users.noreply.github.com>
2022-09-21 13:07:11 -0300

    Update AddProductCart.js

--- ProyectoWEB/controllers/AddProductCart.js
+++ ProyectoWEB/controllers/AddProductCart.js
@@ -3,27 +3,18 @@ const Product = require("../model/productos");
 
 const addProductCart = async (req, res) => {
   const { tipo, nombre, ingredientes, imagenURL, precio } = req.body;
-
-  /* Nos fijamos si tenemos el producto */
   const estaEnProducts = await Product.findOne({ tipo, nombre });
-
-  /* Nos fijamos si todos los campos vienen con info */
   const noEstaVacio = tipo != "" && nombre !== "" && ingredientes !== "" && imagenURL !== "" && precio !== "";
-
-  /* Nos fijamos si el producto ya esta en el carrito */
   const estaEnElCarrito = await Cart.findOne({ tipo, nombre });
 
-  /* Si no tenemos el producto */
+
   if (!estaEnProducts) {
     res.status(400).json({
       mensaje: "Este producto no se encuentra en nuestra base de datos",
     });
-
-    /* Si nos envian algo y no esta en el carrito lo agregamos */
   } else if (noEstaVacio && !estaEnElCarrito) {
     const newProductEnCarro = new Cart({ tipo, nombre, ingredientes, imagenURL,  cantidad: 1, precio});
 
-    /* Y actualizamos la prop EnCarro: true en nuestros productos */
     await Product.findByIdAndUpdate(
       estaEnProducts?._id,
       { EnCarro: true, tipo, nombre, ingredientes, imagenURL, precio },
@@ -38,7 +29,6 @@ const addProductCart = async (req, res) => {
       })
       .catch((error) => console.error(error));
 
-    /* Y si esta en el carrito avisamos */
   } else if (estaEnElCarrito) {
     res.status(400).json({
       mensaje: "El producto ya esta en el carrito",
