326a5fa3d8613fb22e150634a357557a695c5658
yikezzz <66960563+jeanyiquez@users.noreply.github.com>
2022-11-09 02:27:06 -0300

    Add files via upload

--- ProyectoWEB/client/src/componentes/Menu/MenuCaja.jsx
+++ ProyectoWEB/client/src/componentes/Menu/MenuCaja.jsx
@@ -2,7 +2,10 @@ import "./MenuCaja.css"
 
 export function MenuCaja() {
 
-    return <div>
+    return <div class="tablapedidos">
+        <div id="lista" class="screen">
+            Información de los pedidos
+        </div>
         <nav id='menuCaja'>
                 <ul>
                     <li><a href=''>Pedidos</a></li>
@@ -10,10 +13,93 @@ export function MenuCaja() {
                     <li><a href=''>Historial de cobros</a></li>
                 </ul>
         </nav>
+        <table border="i" class="tablapedido">
+            <tr>
+                <th>Numero</th>
+                <th>Fecha</th>
+                <th>Rut</th>
+                <th>Total</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>08/11/2022</td>
+                <td>11.685.879-1</td>
+                <td>$19990</td>
+                <button>Confirmar</button>
+                <button class="rechazar">Rechazar</button>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>09/11/2022</td>
+                <td>19.215.687-1</td>
+                <td>$29990</td>
+                <button>Confirmar</button>
+                <button class="rechazar">Rechazar</button>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>10/11/2022</td>
+                <td>21.275.807-7</td>
+                <td>$59990</td>
+                <button>Confirmar</button>
+                <button class="rechazar">Rechazar</button>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>10/11/2022</td>
+                <td>21.275.807-7</td>
+                <td>$59990</td>
+                <button>Confirmar</button>
+                <button class="rechazar">Rechazar</button>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>10/11/2022</td>
+                <td>21.275.807-7</td>
+                <td>$59990</td>
+                <button>Confirmar</button>
+                <button class="rechazar">Rechazar</button>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>10/11/2022</td>
+                <td>21.275.807-7</td>
+                <td>$59990</td>
+                <button>Confirmar</button>
+                <button class="rechazar">Rechazar</button>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>10/11/2022</td>
+                <td>21.275.807-7</td>
+                <td>$59990</td>
+                <button>Confirmar</button>
+                <button class="rechazar">Rechazar</button>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>10/11/2022</td>
+                <td>21.275.807-7</td>
+                <td>$59990</td>
+                <button>Confirmar</button>
+                <button class="rechazar">Rechazar</button>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td>10/11/2022</td>
+                <td>21.275.807-7</td>
+                <td>$59990</td>
+                <button>Confirmar</button>
+                <button class="rechazar">Rechazar</button>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td>10/11/2022</td>
+                <td>21.275.807-7</td>
+                <td>$59990</td>
+                <button>Confirmar</button>
+                <button class="rechazar">Rechazar</button>
+            </tr>
+        </table>
     </div>
-
 }
-
-
-
-
