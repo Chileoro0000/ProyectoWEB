d55a3f318162a77a412206b5d83fb783b48cdeb2
Chileoro0000 <g.rodrguezcampos@uandresbello.edu>
2022-11-21 00:21:49 -0300

    V23

--- ProyectoWEB/client/src/componentes/Menu/PedidosActuales.jsx
+++ ProyectoWEB/client/src/componentes/Menu/PedidosActuales.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
-import { useEffect } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import { PDFDownloadLink, Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 import "./MenuCaja.css"
 
-export function PedidosActuales () {
+
+export function PedidosActuales() {
     const [Ventas, setVentas] = useState([])
-    const LoadVentas= () => {
+    const LoadVentas = () => {
         fetch("http://localhost:5005/ventasDia")
             .then(res => res.json())
             .then(TodosVentas => setVentas(TodosVentas))
     }
+
+
+    const styles = StyleSheet.create({
+        page: {
+            flexDirection: 'row',
+            backgroundColor: '#E4E4E4'
+        },
+        section: {
+            margin: 10,
+            padding: 10,
+            flexGrow: 1
+        }
+    });
 
     LoadVentas()
     return (
@@ -27,16 +39,34 @@ export function PedidosActuales () {
                         </tr>
                     </thead>
                     <tbody>
-                    {Ventas.map((eachVentas) => (
+                        {Ventas.map((eachVentas) => (
                             <tr>
                                 <td>{eachVentas.comprador_id}</td>
-                                <td>{eachVentas._id}</td> 
+                                <td>{eachVentas._id}</td>
                                 <td>{eachVentas.fecha}</td>
                                 <td>{eachVentas.total}</td>
+                                <PDFDownloadLink document={<Document>
+                                    <Page size="A4" style={styles.page}>
+                                        <View style={styles.section}>
+                                            <Text>{eachVentas.fecha}</Text>
+                                        </View>
+                                        <View style={styles.section}>
+                                            <Text>{eachVentas.total}</Text>
+                                        </View>
+                                        <View style={styles.section}>
+                                            <Text>{eachVentas.comprador_id}</Text>
+                                        </View>
+                                    </Page>
+                                </Document>} fileName="boleta.pdf">
+                                    {({ blob, url, loading, error }) => (loading ? 'Descargando boleta...' : 'Descargar boleta!')}
+                                </PDFDownloadLink>
                             </tr>
                         ))}
                     </tbody>
                 </table>
+            </div>
+            <div>
+
             </div>
         </div>
     );
